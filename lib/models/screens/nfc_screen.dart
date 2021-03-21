b66ad5a4912a8adf1ec6873eb53f18b643f3030c
Filip Szymański <f.szymanski98@gmail.com>
2021-03-21 15:35:55 +0100

    Test NFC formatting

--- lib/models/screens/nfc_screen.dart
+++ lib/models/screens/nfc_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:idrop/models/common/global.dart';
 import 'package:idrop/utils/alerts.dart';
@@ -8,13 +9,14 @@ class NfcScreenModel extends ChangeNotifier {
   BuildContext context;
 
   /* Variables section */
+  StreamSubscription<NDEFTag> streamSubscription;
   bool supportsNfc = false;
   /* Variables section end */
 
   /* Logic section */
   NfcScreenModel(this.context) {
     NFC.isNDEFSupported.then((value) {
-      supportsNfc = value;
+      supportsNfc = true;
       refresh();
     });
   }
@@ -28,13 +30,13 @@ class NfcScreenModel extends ChangeNotifier {
       'user/profile/$accountId',
     );
 
-    NDEFMessage newMessage = NDEFMessage.withRecords([
-      NDEFRecord.uri(path),
-    ]);
-
-    Stream<NDEFTag> stream = NFC.writeNDEF(newMessage, once: true);
-    stream.listen((NDEFTag tag) {
-      showOkAlert(context, 'Your IDrop was successfully activated.');
+    // First format the tag
+    NDEFMessage message = NDEFMessage.withRecords([]);
+    Stream<NDEFTag> stream = NFC.writeNDEF(message, once: true);
+    stream.listen((NDEFTag tag) async {
+      showOkAlert(context, 'NFC tag formatted');
+      await tag.write(NDEFMessage.withRecords([NDEFRecord.uri(path)]));
+      showOkAlert(context, 'NFC tag activated');
     });
   }
   /* Logic section end */
