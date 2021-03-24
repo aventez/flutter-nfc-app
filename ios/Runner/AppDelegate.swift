de227c1a87ebf664968f9f92251a0eef06f90aca
Filip Szymański <f.szymanski98@gmail.com>
2021-03-25 00:30:15 +0100

    iOS boilerplate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let nfcChannel = FluterMethodChannel(name: "idrop.lib/nfc", binaryMessenger: controller.binaryMessenger)
+
+    nfcChannel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      if call.method == "writeNfc" {
+        result(1)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
