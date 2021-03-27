76fe95e3275f23b6c83453bf2d74d83aa2508977
Michał Mańkus <ptasigryp@gmail.com>
2021-03-27 15:08:46 +0100

    iOS working versionn

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,23 +3,37 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let nfcChannel = FluterMethodChannel(name: "idrop.lib/nfc", binaryMessenger: controller.binaryMessenger)
+    
+    private let nfcService = NFCService()
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        let nfcChannel = FlutterMethodChannel(name: "idrop.lib/nfc", binaryMessenger: controller.binaryMessenger)
 
-    nfcChannel.setMethodCallHandler({
-      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-      if call.method == "writeNfc" {
-        result(1)
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
-    })
+        nfcChannel.setMethodCallHandler({ [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            guard let self = self else { return }
+            if call.method == "writeNfc" {
+                print("writeNFc method called")
+                guard
+                    let urlString = call.arguments as? String,
+                    let url = URL(string: urlString)
+                else {
+                    print("couldn't map url from arguments, args: \(String(describing: call.arguments))")
+                    return
+                }
+                
+                print(url)
+                self.nfcService.writeTag(url: url)
+                result(true)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        })
 
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
